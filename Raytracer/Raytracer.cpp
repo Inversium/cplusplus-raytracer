fbfaf4bc877c863176271263f7987be6a32953c5
Inversium <33330019+Inversium@users.noreply.github.com>
2020-11-03 17:36:12 +0300

    functions for bmp image creation

--- Raytracer/Raytracer.cpp
+++ Raytracer/Raytracer.cpp
@@ -2,9 +2,30 @@
 //
 
 #include <iostream>
+#include "math/Headers/mvector.h"
+#include "BMPWriter.h"
+
+#define HEIGHT 1080
+#define WIDTH 1920
+
+void render(vec3* framebuffer)
+{
+    for (size_t i = 0; i < HEIGHT; i++)
+    {
+        for (size_t j = 0; j < WIDTH; j++)
+        {
+            framebuffer[i * WIDTH + j].x = (double) j / WIDTH;
+            framebuffer[i * WIDTH + j].y = (double) i / HEIGHT;
+            framebuffer[i * WIDTH + j].z = 0;
+        }
+    }
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vec3* frame = new vec3[WIDTH * HEIGHT];
+    render(frame);
+    CreateImage(frame, HEIGHT, WIDTH);
 }
 
